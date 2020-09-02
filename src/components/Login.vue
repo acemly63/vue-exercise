882cbc076ce1960e3c46f35ef35c6a5cbf60be0f
acemly63 <acemly63@163.com>
2020-09-02 20:50:01 +0800

    登录页面

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    登录组件
+  <div class="login_container">
+    <div class="login_box"></div>
   </div>
 </template>
 
@@ -8,4 +8,19 @@
 export default {}
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.login_container {
+  background-color: #ccc;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
